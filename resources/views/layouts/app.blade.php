7ed5a4e766517ae9d0cd8181d68550f06973449b
mozessinaga <akunlogis007@gmail.com>
2023-07-03 19:22:55 +0700

    update pemesanan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,14 +22,14 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Wisata Baturaden</a>
+                <a class="navbar-brand" href="{{ route('home') }}">Wisata Baturaden</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link" href="#">Pemesanan</a>
+                        <a class="nav-link" href="{{ route('tickets.purchase') }}">Pemesanan</a>
                         <a class="nav-link" href="#">Pembayaran</a>
                         <a class="nav-link disabled">Tentang Kami</a>
                     </div>
--- resources/views/tickets/purchase.blade.php
+++ resources/views/tickets/purchase.blade.php
@@ -1,16 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Ticket Purchase</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h1 class="mb-0">Ticket Purchase</h1>
+                    </div>
 
-    <form method="POST" action="{{ route('tickets.process') }}">
-        @csrf
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('tickets.process') }}">
+                            @csrf
 
-        <div class="form-group">
-            <label for="quantity">Quantity:</label>
-            <input type="number" name="quantity" id="quantity" class="form-control" min="1" required>
+                            <div class="form-group">
+                                <label for="quantity">Quantity:</label>
+                                <input type="number" name="quantity" id="quantity" class="form-control" min="1" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="total_price">Total Price:</label>
+                                <input type="text" name="total_price" id="total_price" class="form-control" readonly>
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Purchase Tickets</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <button type="submit" class="btn btn-primary">Purchase Tickets</button>
-    </form>
+    <script>
+        // Update total price when the quantity input changes
+        document.getElementById('quantity').addEventListener('input', function() {
+            var quantity = this.value;
+            var totalPrice = 20000 * quantity;
+
+            document.getElementById('total_price').value = 'Rp' + totalPrice.toLocaleString();
+        });
+    </script>
 @endsection
